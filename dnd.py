8d0f5fb9fbf0c12353400579876bce82d174d21d
Max Liles <lileswm@miamioh.edu>
2021-05-01 13:19:36 -0400

    returns all six stats, not working

--- dnd.py
+++ dnd.py
@@ -3,18 +3,30 @@ from random import randint
     ### Determines character stats using 5th Edition method ###
 
 class Stats:
-    def __init__(self, ability):
-        self.ability = ability
+    def __init__(self, character):
+        self.character = character
    ### Rolls 4 d6, removes lowest value and prints out total. ###
     def getStat(self):
         value = []
-        for i in range(4):
-            roll = [randint(1,6)]
-            value.append(roll.pop())
-        value.remove(min(value))
+        score = {'Strength': 0, 'Dexterity': 0, 'Constitution': 0,
+                   'Intelligence': 0, 'Wisdom': 0, 'Charisma': 0,}
+        for k in list(score.items()):
+            for i in range(4):
+                roll = [randint(1,6)]
+                value.append(roll.pop())
+# This is not looping correctly, keeps adding results to value,
+            value.remove(min(value))
         print(value)
-        print(f"Your {self.ability} is {sum(value)}")
-
+        print(sum(value))
+            
+            
+            
+               
+               
+        print(f"{self.character}'s stats are: \n{score}")
 
             
-    
+
+john = Stats('John')
+
+john.getStat()
